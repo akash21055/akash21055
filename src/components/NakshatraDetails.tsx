c22b014bed1de7b3cc63a08efc9f0d8f974b507d
Akash Anande <akash.anande2105@gmail.com>
2026-04-26 15:27:37 +0530

    Fix TypeScript error in NakshatraDetails transition type

--- src/components/NakshatraDetails.tsx
+++ src/components/NakshatraDetails.tsx
@@ -23,7 +23,7 @@ export default function NakshatraDetails() {
       opacity: 1,
       y: 0,
       transition: {
-        type: "spring",
+        type: "spring" as const,
         stiffness: 100,
         damping: 15,
       },
